9e319d8a5dcabab27c379ad98683a1912cacf928
wallowitch2 <wallowitch2@gmail.com>
2023-04-13 14:09:56 +0900

    change view

---
+++ src/view.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('view', { welcomeMessage: '환영합니다 nodejs 입니다' });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
